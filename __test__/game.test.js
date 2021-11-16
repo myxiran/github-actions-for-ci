679b25e15bf8be8e82ac3955f07bf7394f972091
myxiran <74399757+myxiran@users.noreply.github.com>
2021-11-16 19:07:46 +0800

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -20,7 +20,7 @@ describe('Game', () => {
     it('Initializes with two players', async () => {
       expect(game.p1).toBe('Salem')
       expect(game.p2).toBe('Nate')
-    })
+  })
 
     it('Initializes with an empty board', async () => {
       for (let r = 0; r < game.board.length; r++) {
